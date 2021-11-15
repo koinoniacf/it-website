45233239ce536f224d95d94287abb2ffd733863d
JasonCook599 <JasonCook599@users.noreply.github.com>
2021-11-15 15:43:10 -0500

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+const CleanCSS = require("clean-css"); // minify CSS
+const { minify } = require("terser"); // minify JS
+const htmlmin = require("html-minifier"); // minify HTML
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy({"assets": "/"});
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addTransform("minify-output", async function(content, outputPath) {
+        if (outputPath && outputPath.endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        } else if (outputPath && outputPath.endsWith(".css")) {
+            return new CleanCSS().minify(content).styles
+        } else if (outputPath && outputPath.endsWith(".js")) {
+            var result = await minify(content);
+            return result.code
+        } else {
+            return content;
+        }
+    });
+};
